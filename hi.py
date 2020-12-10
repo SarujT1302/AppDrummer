cf39d7df11afb50390eebaeba2706196707c10d1
Saruj <sarujj@example.com>
2020-12-10 14:24:09 +0700

    Song Style

---
+++ hi.py
@@ -0,0 +1,18 @@
+Song_name=input("Enter song name:")
+Song_type=input("Enter type:")
+p=[]
+r=[]
+h=[]
+b=[]
+j=[]
+
+def Pop(s):
+    p.append(s)
+def rock(s):
+    r.append(s)
+def hiphop(s):
+    h.append(S)
+def jazz(s):
+    j.append(s)
+def rb(s):
+    b.append(s)
