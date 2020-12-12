578fb38029b6328e024ea1c6714eb79fa2d79283
Saruj <sarujj@example.com>
2020-12-12 16:16:45 +0700

    Suddenly Metronome

--- Metronome.py
+++ Metronome.py
@@ -10,29 +10,35 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import time
-
 list1=[]
+a=[]
+result=0
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(800, 607)
         MainWindow.setStyleSheet("background:url(:/newPrefix/รูปfrom internet/dark-blue-plain-wall-background_53876-92976.jpg)")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(50, 80, 701, 271))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(30, 100, 741, 271))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.lcdNumber = QtWidgets.QLCDNumber(self.horizontalLayoutWidget)
-        self.lcdNumber.setStyleSheet("color: rgb(255, 0, 0);\n"
-"background-color: rgb(85, 170, 255);\n"
-"alternate-background-color: rgb(255, 255, 255);\n"
-"background-color: rgb(255, 255, 255);\n"
-"background-color: rgb(255, 255, 255);")
-        self.lcdNumber.setObjectName("lcdNumber")
-        self.horizontalLayout.addWidget(self.lcdNumber)
+        self.label_3 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_3.setStyleSheet("color: rgb(255, 255, 127);\n"
+"font: 36pt \"Poor Richard\";")
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout.addWidget(self.label_3)
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setStyleSheet("color: rgb(255, 255, 127);\n"
+"border-color: rgb(200, 14, 42);\n"
+"border-color: rgb(212, 0, 0);\n"
+"gridline-color: rgb(255, 0, 0);\n"
+"font: 72pt \"Poor Richard\";")
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(200, 400, 391, 81))
         self.pushButton.setStyleSheet("background-color: rgb(255, 255, 255);\n"
@@ -40,6 +46,17 @@ class Ui_MainWindow(object):
 "font: 75 16pt \"Rockwell\";\n"
 "background-image: url(:/newPrefix/รูปfrom internet/3025528.jpg);")
         self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 90, 761, 291))
+        self.label_2.setStyleSheet("background-image: url(:/newPrefix/รูปfrom internet/3025528.jpg);")
+        self.label_2.setObjectName("label_2")
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton.setGeometry(QtCore.QRect(390, 510, 20, 21))
+        self.radioButton.setObjectName("radioButton")
+        self.label_2.raise_()
+        self.horizontalLayoutWidget.raise_()
+        self.pushButton.raise_()
+        self.radioButton.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -48,38 +65,46 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        
+        self.pushButton.clicked.connect(self.click)
 
         self.retranslateUi(MainWindow)
-        self.pushButton.clicked.connect(self.lcdNumber.setBinMode)
+        self.pushButton.clicked.connect(self.radioButton.animateClick)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "      Metronome"))
+        self.label.setText(_translate("MainWindow", "  "))
         self.pushButton.setText(_translate("MainWindow", "Click To Count !"))
-        self.pushButton.clicked.connect(self.click)
-
-    def click(self):       
-        if len(list1)<2:
-          p=time.time()
-          list1.append(p)
-          BPM=40
-        if len(list1)==2:
-               p=time.time()
-               list1.append(p)
-               period=list1[1]-list1[0]
-               BPM= int(60//period)
-               list1.clear()
-        self.lcdNumber.display(BPM)
-        print(list1)
-        print(BPM)
+        self.label_2.setText(_translate("MainWindow", "TextLabel"))
+        self.radioButton.setText(_translate("MainWindow", "RadioButton"))
+    def click(self):
+            result=0
+            if len(list1)<2:
+                   p=time.time()
+                   list1.append(p)
+                   BPM=0
         
-        
-                   
-
-           
+            if len(list1)==2 :
+                    p=time.time()
+                    list1.append(p)
+                    period=list1[1]-list1[0]
+                    BPM= int(60//period)
+                    a.append(BPM)
+                    list1.clear()
+            if len(a)>=2 :
+                    for i in a:
+                            result +=i
+                    BPM=result//len(a)
+            print(list1)
+            print(BPM)
+            print(result)
+            self.label.setText(str(BPM))
+            
 import Myimage_metronome_rc
+
+
 
 if __name__ == "__main__":
         import sys
